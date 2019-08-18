4373575cf6803d06d82717b9d33129deea2305c8
sage-jordan <sagemjrodan@gmail.com>
2019-08-18 19:53:11 -0400

    Created AppRouter.js

---
+++ src/components/AppRouter.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+function AppRouter () {
+}
+
+export default AppRouter;
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,11 +1,21 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
 
+  const [character, setCharacter] = useState([]);
+
   useEffect(() => {
     // TODO: Add AJAX/API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios.get("https://rickandmortyapi.com/api/character/")
+      .then(res => {
+        console.log(res);
+        setCharacter(res.results);
+      })
+      .catch(err => console.log(err));
+
   }, []);
 
   return (
