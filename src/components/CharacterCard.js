c9bbd0ccc3183c75fa9f281e80315fbead7017b7
sage-jordan <sagemjrodan@gmail.com>
2019-08-18 22:06:45 -0400

    Finished steps 1 & 2

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Card, Image } from 'semantic-ui-react';
 
 export default function CharacterCard(props) {
+  // console.log(props);
   return (
     <Card>
       <Image src={props.character.image} wrapped ui={false} />
--- src/components/EpisodesList.js
+++ src/components/EpisodesList.js
@@ -1,1 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+import EpisodesCard from './EpisodesCard';
+
+export default function EpisodesList() {
+    // TODO: Add useState to track data from useEffect
+  
+    const [episodes, setEpisodes] = useState([]);
+  
+    useEffect(() => {
+      // TODO: Add AJAX/API Request here - must run in `useEffect`
+      //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+      axios.get("https://rickandmortyapi.com/api/episode/")
+        .then(res => {
+        //   console.log(res.data.results);
+          setEpisodes(res.data.results);
+        })
+        .catch(err => console.log(err));
+  
+    }, []);
+    // console.log(episodes);
+    return (
+      <section className="episode-list grid-view">
+        {episodes.map((episode) => {
+            console.log(episode)
+            return (
+                <EpisodesCard key={episode.id} episode={episode} />
+            )
+        })}
+      </section>
+    );
+  }
