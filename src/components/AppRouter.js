8dace3765a24bd9e649c76f0c628af1b482028f3
sage-jordan <sagemjrodan@gmail.com>
2019-08-18 20:11:40 -0400

    Fixed errors, some TabNav

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import TabNav from './TabNav';
 
 function AppRouter () {
+    return (
+        <div>
+            <TabNav />
+        </div>
+    )
 }
 
 export default AppRouter;
--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -4,5 +4,29 @@ import { NavLink } from "react-router-dom";
 
 // TODO: Add missing tabs below
 export default function TabNav() {
-
+    const tabs = [
+        { menuItem: 'Home', render: () => {
+        return (
+            <Tab.Pane>
+                <Icon name='home' />
+                Home
+            </Tab.Pane>
+        )} },
+        { menuItem: 'Characters', render: () => {
+        return (
+            <Tab.Pane>
+                <Icon name='users' />
+                Characters
+            </Tab.Pane>
+        )} },
+        { menuItem: 'Location', render: () => {
+        return (
+            <Tab.Pane>
+                <Icon name='map marker alternate' />
+                Location
+            </Tab.Pane>
+        )} },
+      ]
+      
+      return (<Tab tabs={tabs} />)
 };
